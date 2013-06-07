08d0baaa939d12c3f5a2442d8fe4f79b161a3e90
Phil Gyford <phil@gyford.com>
2013-06-07 16:05:06 +0100

    Try to fix auth_client scope thing

--- lpgmail/gmail.rb
+++ lpgmail/gmail.rb
@@ -5,19 +5,21 @@ require 'oauth2'
 module LpGmail
   class Gmail
 
-    auth_client = OAuth2::Client.new(
-      ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
-        :site => 'https://accounts.google.com',
-        :authorize_url => '/o/oauth2/auth',
-        :token_url => '/o/oauth2/token'
-      }
-    )
+    def initialize
+      @auth_client = OAuth2::Client.new(
+        ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+          :site => 'https://accounts.google.com',
+          :authorize_url => '/o/oauth2/auth',
+          :token_url => '/o/oauth2/token'
+        }
+      )
+    end
 
 
     # Provides the URL to which we send the user for them to authenticate
     # with Google, and approve access.
     def oauth_authorize_url()
-      auth_client.auth_code.authorize_url(
+      @auth_client.auth_code.authorize_url(
                 :scope => 'https://mail.google.com/',
                 :redirect_uri => url('/return/'),
                 :access_type => 'offline',
@@ -32,7 +34,7 @@ module LpGmail
     # redirect_uri: The URI on this site that has been set as the Return URI.
     # Returns an access_token object with .token and .refresh_token attributes.
     def oauth_get_token(code, redirect_uri)
-      auth_client.auth_code.get_token(code, {
+      @auth_client.auth_code.get_token(code, {
                         :redirect_uri => redirect_uri,
                         :token_method => :post
                       })
@@ -43,7 +45,7 @@ module LpGmail
     # the user signed up.
     # Returns an access_token object with .token and .refresh_token attributes.
     def oauth_get_token_from_hash(refresh_token)
-      OAuth2::AccessToken.from_hash(auth_client,
+      OAuth2::AccessToken.from_hash(@auth_client,
                                     :refresh_token => refresh_token).refresh!
     end
 
