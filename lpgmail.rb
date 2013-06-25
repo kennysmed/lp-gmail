bd341fcae460bd0836211ae25680d9150f4d4cc4
Phil Gyford <phil@gyford.com>
2013-06-25 10:18:07 +0100

    Stop checking for ENV variables

--- lpgmail.rb
+++ lpgmail.rb
@@ -4,8 +4,4 @@ $stdout.sync = true
 require 'thin'
 require 'lpgmail/frontend'
 
-raise 'GOOGLE_CLIENT_ID is not set' if !ENV['GOOGLE_CLIENT_ID']
-raise 'GOOGLE_CLIENT_SECRET is not set' if !ENV['GOOGLE_CLIENT_SECRET']
-
-
 LpGmail::Frontend.run!
