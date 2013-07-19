a81e17936447af6b9e17782d46f3db854cce70d7
Phil Gyford <phil@gyford.com>
2013-07-19 14:45:59 +0100

    Hopefully fixed error code

--- lpgmail/frontend.rb
+++ lpgmail/frontend.rb
@@ -84,13 +84,7 @@ require 'lpgmail/store'
         begin
           gmail.login(refresh_token)
         rescue OAuth2::Error => error
-          error_code = 500
-          # This should be present, but isn't always:
-          p "ERROR " + error.code
-          if error.code
-            error_code = error.code.to_i
-          end
-          halt error_code, " Error when trying to log in (1): #{error.description}"
+          halt error.code.to_i, " Error when trying to log in (1): #{error.description}"
         rescue Net::IMAP::ResponseError => error
           halt 500, " Error when trying to log in (2): #{error}"
         rescue => error
